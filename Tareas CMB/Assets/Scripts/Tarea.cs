2e2b069fffc47c5d0c55a0f546d213744a7b9b28
isaacmarco <isaacmarco@gmail.com>
2020-08-27 14:29:35 +0100

    comenzando tarea

---
+++ Tareas CMB/Assets/Scripts/Tarea.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tarea : MonoBehaviour
+{
+    
+    void Awake()
+    {
+        Inicio();
+    }
+
+    protected virtual void Inicio()
+    {
+
+    }
+}
---
+++ Tareas CMB/Assets/Scripts/TareaTopos.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TareaTopos : Tarea
+{
+    // lista de huecos donde puede aparecer el topo     
+    public GameObject[] huecos; 
+    // lista de disponibilidad de huecos
+    private bool[] huecosDisponibles; 
+
+    protected override void Inicio()
+    {
+    }
+
+    // aparece un topo nuevo 
+    private void NuevoTopo()
+    {
+        // obtener hueco al azar
+        int indiceHueco = Random.Range(0, huecos.Length);
+        // comprobar si esta ocupado
+        if (!huecosDisponibles[indiceHueco])
+        {
+            // mostramos el nuevo topo
+            huecos[indiceHueco].SetActive(true);
+        } else {
+            // no aparece el topo 
+        }
+    }
+
+    // llamado por la interfaz de usuario cuando
+    // miras un hueco 
+    public void GolpearHueco(int indiceHueco)
+    {}
+
+    // se golpea el topo de ese hueco 
+    private void GolpearTopo(Topo topo)
+    {}
+
+}
