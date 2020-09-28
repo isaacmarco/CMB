5e2b4c91e799fe6331d1d0d750859b9f090090fc
isaacmarco <isaacmarco@gmail.com>
2020-09-28 12:52:18 +0100

    refactorizacion, trabajando tarea memory

---
+++ Assets/Scripts/IndicadorEstimuloObjetivo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IndicadorEstimuloObjetivo : MonoBehaviour
+{
+   
+    [SerializeField] Sprite[] estimulos; 
+    [SerializeField] Image indicadorEstimulo; 
+
+    private TareaTopos tarea;
+
+    void Start()
+    {
+        tarea = FindObjectOfType<TareaTopos>();
+    }
+
+    void Update()
+    {
+        if(tarea!=null)
+            indicadorEstimulo.sprite = estimulos[ (int) tarea.Nivel.estimuloObjetivo ];
+    }
+    
+}
