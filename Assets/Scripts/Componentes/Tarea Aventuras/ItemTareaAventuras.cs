8d0fa9fad03b70fb480d1f774748df36fae767ea
isaacmarco <isaacmarco@gmail.com>
2021-03-19 14:02:43 +0000

    sombras

--- Assets/Scripts/Componentes/Tarea Aventuras/ItemTareaAventuras.cs
+++ Assets/Scripts/Componentes/Tarea Aventuras/ItemTareaAventuras.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 
 public class ItemTareaAventuras : MonoBehaviour
 {
-    
+    [SerializeField]
     private SpriteRenderer spriteRenderer; 
     private GestorSpritesAventuras gestorSprites;
 
     public ObjetosAventuras tipo; 
     
+   
+     
+
     void OnTriggerEnter2D(Collider2D col)
     {
+        // solo el jguador puede recoger objetos
+        if(col.gameObject.tag != "Player")
+            return; 
+
         // se puede consumir directamente o coger
         // dependiendo del tipo
         if(tipo == ObjetosAventuras.Corazon || tipo == ObjetosAventuras.Cofre)
@@ -23,13 +30,20 @@ public class ItemTareaAventuras : MonoBehaviour
         }
     }
 
+ 
+
     void Start()
     {
         // creamos las referencias
         gestorSprites = FindObjectOfType<GestorSpritesAventuras>();       
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        // spriteRenderer = GetComponent<SpriteRenderer>();
         // actualizamos el sprite
         spriteRenderer.sprite = gestorSprites.ObtenerSprite(tipo);    
+      
+        iTween.MoveBy(spriteRenderer.gameObject, 
+            iTween.Hash("y", 0.2f, "loopType", "pingPong" 
+        ));
+
     }
 
     private void ProcesarItem()
